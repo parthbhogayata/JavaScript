2833f7172ddba65d4bce67391ed31f29791aaa3f
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-03 18:06:55 +0000

    Variable Declration

---
+++ 01_Basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 123456
+let accountEmail = "parth@gmail.com"
+var accountPassword = "15963"
+accountCity = "Rajkot"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
