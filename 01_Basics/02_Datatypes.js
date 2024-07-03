1be18e6faa6a9d20f3932e6a9a974e0d62654bd0
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-03 18:27:06 +0000

    Datatype discus

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newver version
+
+
+// Datatypes
+
+// 1] -> Primitive Data type 
+
+// number/numeric => 2 to power 52 is range 
+// string => "   "
+// bigint => bigger number
+// boolean => true / false
+// null => standalone value [variable are declare and assigned null value]
+// undefined => variable are  declare but not assign any value.
+// symbol => to identify uniqueness
+
+// 2] -> Object
+let a;
+console.log(typeof a)
+
+// Type of Null is Object
