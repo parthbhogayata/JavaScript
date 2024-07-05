7990c0de201104e397943de8592b18c7e8be807a
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-05 05:40:04 +0000

    Referance Datatyepes

---
+++ 01_Basics/Referancetype.js
@@ -0,0 +1,25 @@
+// Arrays
+
+let myarr = ["parth" , 21]
+
+console.log(myarr);
+console.log(typeof myarr);
+
+// Objects 
+
+let obj = {
+    name : "parth",
+    age : 21,
+    contact : 8849748452
+}
+
+console.log(obj);
+console.log(typeof obj);
+
+// Functions
+
+let myfunc = function(){
+    console.log("Namste Duniya")
+}
+
+myfunc
