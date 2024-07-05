e8b3b7d596367d0d94c2b32cd88c34351ed7cd83
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-05 05:29:33 +0000

    Operations and Comparitions

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,20 @@
+let x = 3;
+const y = x++;
+console.log(`x : ${x} , y: ${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a : ${a} , b: ${b}`);
+
+
+// ********************************** CompressionStream ****************************************
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+console.log("2" === 2); //Comparition Both Value with Their Datatype.
