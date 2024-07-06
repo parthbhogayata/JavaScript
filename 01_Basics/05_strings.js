ed45695055bcfc22bcbcadf2f37b60044f3e7eba
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-06 15:02:16 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,15 @@
+const name = "Parth"
+const rollno = 4052
+
+// console.log(`Hello Viewer my name is : ${name} and my roll no is : ${rollno}`);
+
+const gameCount = new String('Hello World')
+console.log(gameCount.length);
+console.log(gameCount.toUpperCase());
+console.log(gameCount.charAt(5+1));
+console.log(gameCount.indexOf('o'));
+
+const str = gameCount.substring(0,5)
+console.log(str);
+
+// console.log(gameCount.slice(-1,-5));
