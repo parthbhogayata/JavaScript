e7c5ef22ee64ab610fa72466922a30e5df593e9e
Bhogayata Parth <103423046+parthbhogayata@users.noreply.github.com>
2024-07-28 18:21:30 +0530

    JS Basics which will maters as js developer.

---
+++ JS_Basics/Script.js
@@ -0,0 +1,113 @@
+//👉 Word vs Keyword
+
+// Word means No meaning in Language
+    //-> Hello 
+
+// Keyword means Something Predife Meaning
+
+    // for , if , else , var , const ,let ,while, etc..
+    
+
+// Variable and Constant and let  
+
+
+// 👉 hoisting - variable and function are hoistied which means ther declaration is moved on the top of code
+
+    // console.log(a); // UNDEFINE
+    // // console.log(b); NOT DEFINE
+    // var a; //declaration
+    // a = 12; //inialization
+
+    // var a = 10 --> var a; --> a=10; var a moved on top of the code 
+
+// 👉 type in JS
+
+    // 1. Primitive - number , undefined , null , string , boolean  
+        // -> aisi value jiski direct copy mile use primitive hai 
+
+    // 2. Reference = [] , () , {} 
+        // -> aisi koi bhi value jisko copy kane par real copy nahi hota balki us main value ka referance pass ho jata hai vo referene hai
+
+// 👉 REFERENCE
+// var a = [10,20,30,40,50]
+// var b = a;
+// b.pop();
+// console.log(a);
+// console.log(b);
+
+// 👉 Conditionals
+
+    // if , else , else-if
+    // var a = -1;
+    // var b = 12;
+
+    // if(a > b){
+    //     console.log("a is Bigger");
+    // }
+    // else{
+    //     console.log("B is Bigger")
+    // }
+
+
+    // if(a > 0){
+    //     console.log("Positive");
+    // }
+    // else if(a == 0){
+    //     console.log("Zero");
+    // }
+    // else{
+    //     console.log("Negative")
+    // }
+
+// 👉 Loops
+
+    // for , while
+    // repetation of statements of code
+
+    // for(let i = 0 ; i < 5 ; i++){
+    //     console.log(i);
+    // }
+
+    // let b = 0
+    // while(b < 5){
+    //     console.log(b);
+    //     b++;
+    // }
+
+    // for(let j = 0 ; j < 11 ; ++j){
+    //     console.log(j);
+    // }
+
+// 👉 Functions
+
+    // 1. When We want to run our code in future not at the particular time or we can say on same time
+    // 2.Reuse of Code
+    // 3.when we want to run our same code with diffrent data
+
+    // function table(a){
+    //     for(let i = 1 ; i<=10 ; i++){
+    //         console.log(`${a} * ${i} = ${a*i}`);
+    //     }
+    // }
+
+    // table(10)
+
+// 👉 Arrays
+
+    // Multiple value store in single variable 
+    // Index start with 0
+    // Arrays is Object [referance] data Type
+
+    // --> Push , Pop , Shift , Unshift
+    
+    // var arr = [10,20,30,40,50]
+    // arr.push(100)
+    // console.log(arr);
+    // arr.pop()
+    // console.log(arr);
+    // arr.shift()
+    // console.log(arr);
+    // arr.unshift("hello")
+    // console.log(arr);
+
+
